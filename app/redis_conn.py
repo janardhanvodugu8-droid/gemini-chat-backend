dc34755185b5750da75081ca2b0e85031589f58e
janardhanvodugu8-droid <janardhanvodugu8@gmail.com>
2025-08-06 22:19:52 +0530

    Update redis_conn.py

--- app/redis_conn.py
+++ app/redis_conn.py
@@ -6,7 +6,7 @@ redis_url = os.getenv("REDIS_URL")
 
 parsed_url = urlparse(redis_url)
 
-redis_conn = redis.Redis(
+redis = redis.Redis(
     host=parsed_url.hostname,
     port=parsed_url.port,
     password=parsed_url.password,
