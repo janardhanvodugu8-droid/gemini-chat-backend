2685e2a7e9cc2812fb6b9d3e0d0a127588ae89f7
janardhanvodugu8-droid <janardhanvodugu8@gmail.com>
2025-08-06 21:58:00 +0530

    Update redis_conn.py

--- app/redis_conn.py
+++ app/redis_conn.py
@@ -2,14 +2,15 @@ import os
 import redis
 from urllib.parse import urlparse
 
-redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
+redis_url = os.getenv("REDIS_URL")
 
 parsed_url = urlparse(redis_url)
 
-redis = redis.Redis(
+redis_conn = redis.Redis(
     host=parsed_url.hostname,
     port=parsed_url.port,
     password=parsed_url.password,
-    db=0,
+    ssl=True,  # Use SSL for Redis Cloud
     decode_responses=True
 )
+
